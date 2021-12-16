0bf3afd85a1fff8a94c029145a3e12598ae972c8
Anh-Khoa Huynh <khoakun@gmail.com>
2021-12-16 16:23:54 +0100

    init project

---
+++ src/main/java/fr/anh/cinemademo/CinemademoApplication.java
@@ -0,0 +1,13 @@
+package fr.anh.cinemademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemademoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/anh/cinemademo/CinemademoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.anh.cinemademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemademoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
